f3311c5819a5443abc471c62d7ccd9e763672330
rob-foulkrod <89028299+rob-foulkrod@users.noreply.github.com>
2021-12-01 16:17:38 -0500

    fixing it again

--- apis/user-java/src/main/java/io/swagger/model/InlineResponseDefault.java
+++ apis/user-java/src/main/java/io/swagger/model/InlineResponseDefault.java
@@ -1,4 +1,10 @@
+package io.swagger.model;
 
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.constraints.*;
 
 /**
  * InlineResponseDefault
